225c98cd3f3e0c6cad6c891775cc4dd16a6b90b5
ParkingOath-tech <adam@parkingoath.com>
2026-04-22 09:29:47 +1000

    Remove Sydney reference from cta

--- constants/cta.ts
+++ constants/cta.ts
@@ -1,5 +1,5 @@
 export const CTA_CONTENT = {
     title: "Launching soon",
-    description: "ParkingOath is preparing to launch in Sydney. Early access members will be first to use the parking app when it goes live. Parking, handled. A simpler way to find parking in Sydney.",
+    description: "ParkingOath is preparing to launch! Early access members will be first to use the parking app when it goes live. Parking, handled. A simpler way to find parking.",
     cta: "Get Early Access",
 };
